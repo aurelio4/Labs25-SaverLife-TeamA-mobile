2d92bd0383d4ad65add106b624b35524215ca124
aurelio <aurelioarcabascio@gmail.com>
2020-09-14 19:59:35 -0400

    added get version function

--- App.js
+++ App.js
@@ -1,7 +1,23 @@
 import React from 'react';
-import {SafeAreaView, View, Text, StatusBar} from 'react-native';
+import Styles from './styles/Styles';
+import {SafeAreaView, View, Text, StatusBar, Platform} from 'react-native';
 
 const App = () => {
+  const getVersion = () => {
+    /* if iphone is higher model than iphone 10
+        statusbar outside safeareaview
+      
+      same for android(?)
+    */
+    if (Platform.OS === 'ios') {
+      const majorVersionIOS = parseInt(Platform.Version, 10);
+      console.log(Platform.Version);
+      if (majorVersionIOS <= 9) {
+        console.log(Platform.Version);
+      }
+    }
+  };
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
