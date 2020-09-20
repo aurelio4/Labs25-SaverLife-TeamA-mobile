87ad60ff7afbe2b27a72ef066b6269fbcbe79705
aurelio <aurelioarcabascio@gmail.com>
2020-09-20 14:45:50 -0400

    mobile nav now reflects colors of web view nav

--- src/SaverLifeApp.js
+++ src/SaverLifeApp.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
 
 const SaverLifeApp = () => {
-    return (
-        <View style={styles.container}>
-            <Text style={{fontSize: 30}}>Hello, World</Text>
-        </View>
-    )
-}
+  return (
+    <View style={styles.container}>
+      <Text style={{fontSize: 30}}>Hello, World</Text>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
-})
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
-export default SaverLifeApp
+export default SaverLifeApp;
--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -1,37 +1,30 @@
-import React from 'react'
-import { StyleSheet, SafeAreaView, Text, View, ScrollView } from 'react-native'
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
-import SaverLifeApp from '../../SaverLifeApp'
-import LoadingComponent from './LoadingComponent'
-import BudgetPage from '../../pages/BudgetPage.js'
-import PastSpendingPage from '../../pages/PastSpendingPage.js'
-import NetIncomePage from '../../pages/NetIncomePage.js'
-import GoalProgressBar from './GoalProgressBar'
+import SaverLifeApp from '../../SaverLifeApp';
+import BudgetPage from '../../pages/BudgetPage.js';
+import PastSpendingPage from '../../pages/PastSpendingPage.js';
+import NetIncomePage from '../../pages/NetIncomePage.js';
+import GoalProgressBar from './GoalProgressBar';
+
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Navbar = () => {
-    const Tab = createBottomTabNavigator()
+  const Tab = createBottomTabNavigator();
 
-    return (
-        
-        <>
-            <GoalProgressBar />
-            <Tab.Navigator>
-                <Tab.Screen name="Home" component={SaverLifeApp} />
-                <Tab.Screen name="Budget" component={BudgetPage} />
-                <Tab.Screen name="Past Spending" component={PastSpendingPage} />
-                <Tab.Screen name="NetIncome" component={NetIncomePage} />
-            </Tab.Navigator>
-        </>
-    )
-}
+  return (
+    <>
+      <GoalProgressBar />
+      <Tab.Navigator
+		tabBarOptions={{ activeTintColor:'#c01089', inactiveTintColor: '#ecb7db' }}>
+		<Tab.Screen name="Home" component={SaverLifeApp} options={{ tabBarLabel: 'Home', tabBarIcon: ({ color }) => ( <Ionicons name="home" size={20} color={color} /> ) }} />
+        <Tab.Screen name="Budget" component={BudgetPage} options={{ tabBarLabel: 'Budget', tabBarIcon: ({ color }) => ( <Ionicons name="bar-chart" size={20} color={color} /> ) }} />
+        <Tab.Screen name="Past Spending" component={PastSpendingPage} options={{ tabBarLabel: 'Past Spending', tabBarIcon: ({ color }) => ( <Ionicons name="pie-chart" size={20} color={color} /> ) }} />
+        <Tab.Screen name="Net Income" component={NetIncomePage} options={{ tabBarLabel: 'Net Income', tabBarIcon: ({ color }) => ( <Ionicons name="stats-chart" size={20} color={color} /> ) }} />
+      </Tab.Navigator>
+    </>
+  );
+};
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    }
-})
-
-export default Navbar
+export default Navbar;
