5a427c201331ac598280f3b868703f9daf6730f2
Juan Gerardo Madero Flores <juan@Juans-MacBook.attlocal.net>
2020-09-23 12:03:12 -0700

    progress

--- src/SaverLifeApp.js
+++ src/SaverLifeApp.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 
 const SaverLifeApp = () => {
     return (
         <View style={styles.container}>
-            <Text style={{fontSize: 30}}>Hello, World</Text>
+            <TouchableOpacity style={styles.opacity}><Text style={{color: 'white', fontSize: 20}}>Logout</Text></TouchableOpacity>
         </View>
     )
 }
@@ -14,6 +14,16 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center',
+        backgroundColor: '#fff'
+    },
+    opacity: {
+        backgroundColor: '#c01089',
+        width: 120,
+        height: 70,
+        color: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 15
     }
 })
 
